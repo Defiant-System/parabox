436b7f986bd71e98dbc5d753a59f7c776970cc43
Hakan Bilgin <hbi99@hotmail.com>
2024-02-15 22:37:42 +0100

    added background parallax

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,7 +1,7 @@
 
 let Test = {
 	init(APP) {
-		setTimeout(() => window.find(`.toolbar-tool_[data-arg="2"]`).trigger("click"), 100);
+		// setTimeout(() => window.find(`.toolbar-tool_[data-arg="2"]`).trigger("click"), 100);
 		// setTimeout(() => Game.checkWin(), 1000);
 		// setTimeout(() => window.find(`.exit`).addClass("ready"), 1000);
 		// setTimeout(() => APP.content.addClass("game-won"), 1500);
