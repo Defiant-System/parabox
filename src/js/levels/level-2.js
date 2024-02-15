01dd63ee29e3d47b695838e9eef01bbaff2bf97e
Hakan Bilgin <hbi99@hotmail.com>
2024-02-15 11:50:13 +0100

    Update level-2.js

--- src/js/levels/level-2.js
+++ src/js/levels/level-2.js
@@ -7,14 +7,14 @@ Level[1] = {
 	void: [{ y: 7, x: 2 }, { y: 7, x: 6 }],
 	block: [{ y: 3, x: 4, color: "brown" }, { y: 4, x: 4, color: "brown" }],
 	walls: [
-		[{ key: "NE" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "NW" }],
-		[{ key: "WE", sub: ["NW-NE"] }, {}, {}, {}, {}, { key: "E", sub: ["NE-NW"] }, { key: "F" }, { key: "F" }, { key: "W" }],
-		[{ key: "WE" }, {}, {}, {}, {}, { key: "E" }, { key: "F" }, { key: "F" }, { key: "W" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "SE" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "WS", sub: ["NE-WS"] }],
+		[{ key: "NE" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "NW" }],
+		[{ key: "E" }, { key: "F" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "W" }],
+		[{ key: "E" }, { key: "W", sub: ["NW-NE"] }, {}, {}, {}, { key: "SE", sub: ["NE-NW"] }, { key: "S" }, { key: "F" }, { key: "W" }],
+		[{ key: "E" }, { key: "W" }, {}, { key: "NWE" }, {}, {}, {}, { key: "E", sub: ["NE-NW"] }, { key: "W" }],
+		[{ key: "E" }, { key: "W" }, {}, { key: "WSE" }, {}, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "E" }, { key: "W" }, {}, {}, {}, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "E" }, { key: "W" }, {}, {}, {}, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "E" }, { key: "W" }, {}, {}, {}, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "SE" }, { key: "S" }, { key: "NS", sub: ["NE-ES"] }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "S", sub: ["NE-WS"] }, { key: "WS" }],
 	]
 };
