6734fd3e92ec2cb3727c3b74a4bcff2ef1c1b8cd
Hakan Bilgin <hbi99@hotmail.com>
2024-02-16 09:39:52 +0100

    added editor dnd for wall paint

--- src/js/areas/editor.js
+++ src/js/areas/editor.js
@@ -1,20 +1,50 @@
 
-// parabox.content
+// parabox.editor
 
 {
 	init() {
 		// fast references
 		this.els = {
+			doc: $(document),
 			el: window.find(`.editor-view[data-area="editor"]`),
 		};
+		// bind event handlers
+		this.els.el.on("mousedown", this.paintWall);
 	},
 	dispatch(event) {
 		let APP = parabox,
-			Self = APP.content,
+			Self = APP.editor,
 			el;
 		// console.log(event);
 		switch (event.type) {
 			case "custom-event":
+				break;
+		}
+	},
+	paintWall(event) {
+		let APP = parabox,
+			Self = APP.editor,
+			Drag = Self.drag;
+		// console.log(opt);
+		switch (event.type) {
+			// native events
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+
+				// cover content
+				APP.content.addClass("cover");
+				// bind event handlers
+				Self.els.doc.on("mousemove mouseup", Self.paintWall);
+				break;
+			case "mousemove":
+				console.log(123);
+				break;
+			case "mouseup":
+				// cover content
+				APP.content.removeClass("cover");
+				// bind event handlers
+				Self.els.doc.off("mousemove mouseup", Self.paintWall);
 				break;
 		}
 	}
