04361f3c3ae7a12f60de3d2fa6297d1780d9a8e6
Hakan Bilgin <hbi99@hotmail.com>
2024-02-16 22:23:55 +0100

    ui tweaks for mini maps

--- src/js/levels/level-4.js
+++ src/js/levels/level-4.js
@@ -5,7 +5,7 @@ Level[4] = {
 	player: { y: 7, x: 1 },
 	exit: { y: 6, x: 4 },
 	void: [{ y: 1, x: 6 }, { y: 6, x: 6 }],
-	block: [{ y: 4, x: 2, color: "yellow" }, { y: 3, x: 3, color: "blue" }],
+	block: [{ y: 4, x: 2, color: "yellow" }, { y: 3, x: 3, mini: 4.1 }],
 	walls: [
 		[{ key: "NE" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "NS" }, { key: "N" }, { key: "NW" }],
 		[{ key: "E" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "W", sub: ["NW-NE"] }, {}, { key: "E", sub: ["NE-NW"] }, { key: "W" }],
@@ -16,5 +16,17 @@ Level[4] = {
 		[{ key: "WE", sub: ["NW-NE"] }, {}, {}, { key: "WE", sub: ["NW-NE"] }, {}, {}, {}, { key: "E" }, { key: "W" }],
 		[{ key: "WE" }, {}, {}, { key: "E" }, { key: "N", sub: ["NE-ES"] }, { key: "N" }, { key: "N" }, { key: "F", sub: ["NE-WS"] }, { key: "W" }],
 		[{ key: "SE" }, { key: "NS", sub: ["NE-ES"] }, { key: "NS" }, { key: "S", sub: ["NE-WS"] }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "WS" }],
+	]
+};
+
+Level[4.1] = {
+	bg: "#136",
+	filter: "sepia(1) hue-rotate(175deg) saturate(2) brightness(.65)",
+	walls: [
+		[{ key: "NE" }, { key: "NWS" }, {}, { key: "NSE" }, { key: "NW" }],
+		[{ key: "SE" }, { key: "WS" }, {}, { key: "E" }, { key: "W" }],
+		[{}, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "NWE" }, {}, {}, { key: "E" }, { key: "W" }],
+		[{ key: "SE" }, { key: "NS", sub: ["NE-ES"] }, { key: "NS" }, { key: "S", sub: ["NE-WS"] }, { key: "WS" }],
 	]
 };
