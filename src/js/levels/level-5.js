76e4348a35baf8b960ba707cb6c486b0ccfb029d
Hakan Bilgin <hbi99@hotmail.com>
2024-02-17 18:19:54 +0100

    preparing level 6

--- src/js/levels/level-5.js
+++ src/js/levels/level-5.js
@@ -5,7 +5,7 @@ Level[5] = {
 	player: { y: 3, x: 7 },
 	exit: { y: 3, x: 6 },
 	void: [{ y: 5, x: 4 }],
-	block: [{ y: 5, x: 2, color: "blue" }],
+	block: [{ y: 5, x: 2, mini: 5.1 }],
 	walls: [
 		[{ key: "NE" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "NW" }],
 		[{ key: "E" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "W" }],
@@ -16,5 +16,15 @@ Level[5] = {
 		[{ key: "WE" }, {}, {}, {}, { key: "NE" }, { key: "N" }, { key: "N" }, { key: "N" }, { key: "W", sub: ["NE-WS"] }],
 		[{ key: "E" }, { key: "N", sub: ["NE-ES"] }, { key: "N" }, { key: "N" }, { key: "F", sub: ["NE-WS"] }, { key: "F" }, { key: "F" }, { key: "F" }, { key: "W" }],
 		[{ key: "SE" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "S" }, { key: "WS" }],
+	]
+};
+
+Level[5.1] = {
+	bg: "#339",
+	filter: "sepia(1) hue-rotate(195deg) saturate(2) brightness(.75)",
+	walls: [
+		[{ key: "NWE" }, {}, { key: "NWSE" }],
+		[{ key: "WE" }, {}, {}],
+		[{ key: "SE" }, { key: "NS", sub: ["NE-ES"] }, { key: "NWS" }],
 	]
 };
--- src/js/levels/level-6.js
+++ src/js/levels/level-6.js
@@ -1,20 +1,32 @@
 
 Level[6] = {
-	bg: "#666",
+	bg: "#626262",
 	filter: "none",
-	player: { y: 3, x: 2 },
-	exit: { y: 5, x: 7 },
-	void: [{ y: 1, x: 2 }],
-	block: [{ y: 3, x: 3, color: "yellow" }],
+	player: { y: 3, x: 7 },
+	exit: { y: 3, x: 6 },
+	void: [{ y: 5, x: 4 }],
+	block: [{ y: 5, x: 2, mini: 5.1 }],
 	walls: [
-		[{ key: "NE" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NW" }],
-		[{ key: "WE", sub: ["NW-NE"] }, {}, {}, {}, {}, {}, {}, {}, { key: "WE", sub: ["NE-NW"] }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
-		[{ key: "SE" }, { key: "NS", sub: ["NE-ES"] }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "WS", sub: ["NE-WS"] }],
+		[{ key: "NE" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NW" }],
+		[{ key: "WE", sub: ["NW-NE"] }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE", sub: ["NE-NW"] }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "WE" }, {}, {}, {}, {}, {}, {}, {}, {}, {}, { key: "WE" }],
+		[{ key: "SE" }, { key: "NS", sub: ["NE-ES"] }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "NS" }, { key: "WS", sub: ["NE-WS"] }],
+	]
+};
+
+Level[6.1] = {
+	bg: "#339",
+	filter: "sepia(1) hue-rotate(195deg) saturate(2) brightness(.75)",
+	walls: [
+		[{ key: "NWE" }, {}, { key: "NWSE" }],
+		[{ key: "WE" }, {}, {}],
+		[{ key: "SE" }, { key: "NS" }, { key: "NWS" }],
 	]
 };
