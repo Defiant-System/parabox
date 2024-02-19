fab1f159bbb7e94a14b83c67bb56d1c4c1d35e92
Hakan Bilgin <hbi99@hotmail.com>
2024-02-19 14:05:01 +0100

    added levels 8 & 9

--- src/js/levels/world-1/level-8.js
+++ src/js/levels/world-1/level-8.js
@@ -1,16 +1,35 @@
 
 Level[8] = {
-	grid: 4,
-	bg: "#829",
-	filter: "sepia(1) hue-rotate(245deg) saturate(1.5) brightness(.85)",
-	player: { y: 1, x: 1 },
-	exit: { y: 2, x: 1 },
-	void: [{ y: 2, x: 2 }],
-	block: [{ y: 1, x: 2, color: "yellow" }],
+	grid: 9,
+	bg: "#454",
+	filter: "sepia(1) hue-rotate(65deg) saturate(1.25) brightness(.75)",
+	player: { y: 2, x: 2 },
+	exit: { y: 4, x: 2 },
+	block: [
+		{ y: 1, x: 5, mini: 8.1 },
+		{ y: 4, x: 4, color: "yellow" }
+	],
 	walls: [
-		[{ key: "NSE" }, { key: "NS" }, { key: "NS" }, { key: "NW" }],
-		[{}, {}, {}, { key: "WE", sub: ["NE-NW"] }],
-		[{ key: "NWE" }, {}, {}, { key: "WE" }],
-		[{ key: "SE" }, { key: "NWS", sub: ["NE-ES"] }, {}, { key: "WSE" }],
+		[{ "key": "NE" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NW" }],
+		[{ "key": "WE","sub":["NW-NE"] }, {}, {}, {}, {}, {}, { "key": "WE","sub":["NE-NW"] }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "SE" }, { "key": "NS","sub":["NE-ES"] }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "WS","sub":["NE-WS"] }]
+	]
+};
+
+Level[8.1] = {
+	grid: 5,
+	bg: "#24a",
+	filter: "sepia(1) hue-rotate(175deg) saturate(1.75) brightness(.8)",
+	void: [{ y: 3, x: 1 }],
+	walls: [
+		[{ "key": "NE" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NW" }],
+		[{ "key": "WSE","sub":["NW-NE"] }, {}, {}, {}, { "key": "WE","sub":["NE-NW"] }],
+		[{}, {}, { "key": "NWSE" }, {}, { "key": "WE" }],
+		[{ "key": "NWE" }, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "SE" }, { "key": "NWS","sub":["NE-ES"] }, {}, { "key": "NSE" }, { "key": "WS","sub":["NE-WS"] }],
 	]
 };
--- src/js/levels/world-1/level-9.js
+++ src/js/levels/world-1/level-9.js
@@ -1,19 +1,35 @@
 
-Level[8] = {
+Level[9] = {
 	grid: 7,
-	bg: "#829",
-	filter: "sepia(1) hue-rotate(245deg) saturate(1.5) brightness(.85)",
-	player: { y: 1, x: 1 },
-	exit: { y: 2, x: 1 },
-	void: [{ y: 2, x: 2 }],
-	block: [{ y: 1, x: 2, color: "yellow" }],
+	bg: "#795d7d",
+	filter: "sepia(.5) hue-rotate(245deg) saturate(2) brightness(.9)",
+	player: { y: 2, x: 4 },
+	void: [{ y: 4, x: 3 }],
+	block: [
+		{ y: 2, x: 2, color: "yellow" },
+		{ y: 4, x: 4, mini: 9.1 },
+	],
 	walls: [
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
-		[{}, {}, {}, {}, {}, {}, {}],
+		[{ "key": "NE" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NW" }],
+		[{ "key": "WE","sub":["NW-NE"] }, {}, {}, {}, {}, {}, { "key": "WE","sub":["NE-NW"] }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "WE" }, {}, {}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "WE" }, {}, {}, {}, {}, { "key": "NE" }, { "key": "W","sub":["NE-WS"] }],
+		[{ "key": "WE" }, {}, {}, {}, { "key": "NE" }, { "key": "F","sub":["NE-WS"] }, { "key": "W" }],
+		[{ "key": "SE" }, { "key": "NS","sub":["NE-ES"] }, { "key": "NS" }, { "key": "NS" }, { "key": "S","sub":["NE-WS"] }, { "key": "S" }, { "key": "WS" }]
+	]
+};
+
+Level[9.1] = {
+	grid: 5,
+	bg: "#24a",
+	filter: "sepia(1) hue-rotate(175deg) saturate(1.75) brightness(.8)",
+	exit: { y: 2, x: 3 },
+	walls: [
+		[{ "key": "NE" }, { "key": "NS" }, { "key": "NS" }, { "key": "NS" }, { "key": "NW" }],
+		[{ "key": "WSE","sub":["NW-NE"] }, {}, {}, {}, { "key": "WE","sub":["NE-NW"] }],
+		[{}, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "NWE" }, {}, {}, {}, { "key": "WE" }],
+		[{ "key": "SE" }, { "key": "NS","sub":["NE-ES"] }, { "key": "NS" }, { "key": "NS" }, { "key": "WS","sub":["NE-WS"] }],
 	]
 };
